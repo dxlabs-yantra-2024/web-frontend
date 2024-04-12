b015c9623e640f74ea575ac10e6c0c3cbdfa5bd9
Ayush Mhetre <mhetreayush1719@gmail.com>
2024-04-12 22:20:34 +0530

    ✨ Project setup

---
+++ src/providers/QueryClientProvider.tsx
@@ -0,0 +1,15 @@
+// app/providers.jsx
+"use client";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  const queryClient = new QueryClient();
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+}
