477dc58a931cbd88113a20c2b372ff01c5f5b455
Ayush Mhetre <mhetreayush1719@gmail.com>
2024-04-13 23:14:31 +0530

    🐛 ENV Fix

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -1,4 +1,4 @@
 export const ENV = {
-  APP_ENV: process.env.APP_ENV || "development",
-  API_ROOT: process.env.API_ROOT || "http://localhost:8080",
+  APP_ENV: process.env.NEXT_PUBLIC_APP_ENV || "development",
+  API_ROOT: process.env.NEXT_PUBLIC_API_ROOT || "http://localhost:8080",
 };
