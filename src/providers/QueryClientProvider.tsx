a1b3a15cd6d3f4b3d77713d16dda6c45ba7b867c
Ayush <mhetreayush1719@gmail.com>
2024-07-03 01:11:07 +0530

    🐛fix: add children

--- src/providers/QueryClientProvider.tsx
+++ src/providers/QueryClientProvider.tsx
@@ -9,6 +9,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
   const queryClient = new QueryClient();
   return (
     <QueryClientProvider client={queryClient}>
+      {children}
       {ENV.APP_ENV === "development" && (
         <ReactQueryDevtools initialIsOpen={false} />
       )}
